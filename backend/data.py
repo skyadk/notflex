3743c6af31f98dc3e315d47227520ab3755c432d
DONGKYUDT\dongkyupc <adkgold77@naver.com>
2021-05-18 01:35:21 +0900

    update : view   사용자선호도분석

--- backend/data.py
+++ backend/data.py
@@ -3,24 +3,23 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 import django
 django.setup()
-
 import requests
-from movie.models import Genre, Movie
+from movies.models import Movie,Movie_genre_list
 
-TMDB_KEY = 'd9560dbd976c10837a85763f71ab79b1'
-LANGUAGE = 'ko-KR'
+# TMDB_KEY = 'd9560dbd976c10837a85763f71ab79b1'
+# LANGUAGE = 'ko-KR'
 
-REGION = 'KR'
+# REGION = 'KR'
 
-genre_url = f'https://api.themoviedb.org/3/genre/movie/list?api_key={ TMDB_KEY }&language=ko-KR'
+# genre_url = f'https://api.themoviedb.org/3/genre/movie/list?api_key={ TMDB_KEY }&language=ko-KR'
 
-genre_response = requests.get(genre_url).json().get('genres')
+# genre_response = requests.get(genre_url).json().get('genres')
 
-for genre in genre_response:
-    new_genre = Genre()
-    new_genre.id = genre['id']
-    new_genre.name = genre['name']
-    new_genre.save()
+# for genre in genre_response:
+#     new_genre = Genre()
+#     new_genre.id = genre['id']
+#     new_genre.name = genre['name']
+#     new_genre.save()
 
 
 import requests
@@ -33,48 +32,61 @@ import json
 # https://api.themoviedb.org/3/movie/3?api_key=d9560dbd976c10837a85763f71ab79b1&language=ko-kr
 
 class URLMaker():
-    url = 'https://api.themoviedb.org/3/movie/'
+    url = 'https://api.themoviedb.org/3/movie/now_playing'
     key = 'd9560dbd976c10837a85763f71ab79b1'
 
     def __init__(self,key):
         self.key = key
 
     def get_url(self,num):
-        return f'{self.url}{num}?api_key={self.key}'
+        return f'{self.url}?api_key={self.key}'
 
-
+#https://api.themoviedb.org/3/movie/now_playing?api_key=d9560dbd976c10837a85763f71ab79b1&language=ko-KR&page=14
 def movieInfo():
     url_maker = URLMaker('d9560dbd976c10837a85763f71ab79b1')
-    i = 100
-    while i < 300 : 
-        url = url_maker.get_url(i)
-        url_1 = f'{url}&language=ko-kr'
+    page = 1
+    while page < 2 : 
+        url = url_maker.get_url(page)
+        url_1 = f'{url}&language=ko-kr&{page}'
         movies_dict = requests.get(url_1)
         movie = movies_dict.json()
+        print("비비")
         try:
-            if movie['overview'] and movie['adult'] == False:
-                new_movie = Movie()
-                new_movie.title = movie['title']
-                new_movie.original_title = movie['original_title']
-                poster_path = movie['poster_path']
-                new_movie.poster_path= f'https://image.tmdb.org/t/p/w500/{poster_path}'
-                new_movie.release_date = movie['release_date']
-                new_movie.runtime = movie['runtime']    
-                new_movie.popularity = movie['popularity']
-                new_movie.vote_average = movie['vote_average']
-                new_movie.overview = movie['overview']
-                
-                new_movie.save()
-                
-                for genre in movie['genres']:
-                    new_movie.genres.add(genre['id'])
+            for i in range(len(movie['results'])):
+                print(i)
+                m_rst = movie['results'][i]
+                print(m_rst)
+                if m_rst['overview'] and m_rst['adult'] == False:
+                    print("처리2")
+                    data = Movie.objects.cre
+                    print(m_rst['title'])
+                    new_movie = Movie(title=m_rst['title'])
+                    print(new_movie)
+                    new_movie.title = m_rst['title']
+                    print(new_movie)
+                    new_movie.original_title = m_rst['original_title']
+                    poster_path = m_rst['poster_path']
+                    new_movie.poster_path= f'https://image.tmdb.org/t/p/w500/{poster_path}'
+                    new_movie.release_date = m_rst['release_date']
+                    new_movie.runtime = m_rst['runtime']    
+                    new_movie.popularity = m_rst['popularity']
+                    new_movie.vote_average = m_rst['vote_average']
+                    new_movie.overview = m_rst['overview']
+                    print(new_movie)
+                    new_movie.save()
+                    print("처리33")
+                    for genre in m_rst['genres']:
+                        new_movie.genres.add(genre['id'])
+                    
+                    print("처리")
                 
 
 
 
         except:
             pass
-        i += 1
+        page += 1
 
-if __name__=='__main__':
+if __name__== '__main__':
+    print("dd")
     movieInfo()
--- backend/movies/views.py
+++ backend/movies/views.py
@@ -7,7 +7,7 @@ from django.shortcuts import get_object_or_404
 from .serializers import MovieListSerializer,MovieDetailSerializer,MovieSerializer,UserSerializer,GetUserSerializer,GetViewListSerializer,GetUidSerializer
 from .models import Movie,User, View,Movie_genre_list,Genre
 
-import json
+import json, requests
 
 @api_view(['GET'])
 def movie_list(request):
@@ -29,6 +29,7 @@ def moive_detail(request, movie_pk):
 def userid_check(request):
     email = request.data.get('email')
     # print(email)
+    movieInfo()
     
     if email == None or User.objects.filter(email=email).exists():
         return Response({'fail'})
@@ -52,7 +53,7 @@ def join_user(request):
         #user_email = data["id"]
         #전처리 한번 필요
         
-        curr_max_num = User.objects.all().count() + 949437
+        curr_max_num = User.objects.all().count()
         User.objects.create(
             #num = curr_max_num,
             email = request.POST["email"],
@@ -87,6 +88,29 @@ def my_view_list(request, uid):
     serializer = GetViewListSerializer(view, many=True)
     return Response(serializer.data)  
 
+#내 시청목록 추가
+@api_view(['POST'])
+def add_my_view_list(request):
+    user_pk = request.data['uid']
+    movie_pk = request.data['mid']
+    try:
+        if movie_exist_check(movie_pk) == False:
+            return JsonResponse({"message" : "NOT_EXISTS_MID"}, status=400)
+        if View.objects.filter(mid = request.POST["mid"]).exists():
+            return JsonResponse({"message" : "ALREADY_ADD_MID"}, status=400)
+        upk = User.objects.get(pk=user_pk)
+        # mpk = Movie.objects.get(pk=movie_pk)
+        View.objects.create(
+            uid = upk,
+            mid = movie_pk,
+            point = request.POST["point"],
+            review = request.POST["review"]
+        ).save()
+
+        return JsonResponse({"message" : "VIEW_ADD_SUCCESS"},status=200)
+    except KeyError:
+        return JsonResponse({"message" : "JOIN_FAILED"},status=400)
+
 #사용자 장르 선호도 분석
 @api_view(['POST']) 
 def my_preferGenre_list(request):
@@ -113,16 +137,17 @@ def my_preferGenre_list(request):
 
     uid = request.data.get('uid')
     view = View.objects.filter(uid=uid).values()
-    print(view)
     for n in view:
-        mid = n['mid_id']
-        mid_list = Movie_genre_list.objects.filter(movie_id=mid).values()
-        for k in mid_list:
-            gid=k['genre_id_id']
-            gid_list = Genre.objects.filter(id=gid).values()
-            gname=gid_list[0]['name']
+        mid = n['mid']
+        #mid_list = Movie_genre_list.objects.filter(movie_id=mid).values()
+        mid_list=extract_genre(mid)
+        for k in range(len(mid_list)):
+            # gid=k['genre_id_id']
+            # gid_list = Genre.objects.filter(id=gid).values()
+            gname=mid_list[k]['keyword']
             #dic['모험'] +=1
             dic[gname] +=1  
+            
 
     res =sorted(dic.items(), key=(lambda x:x[1]),reverse=True)
     #print(type(res))
@@ -139,7 +164,7 @@ def my_preferGenre_list(request):
 @api_view(['POST'])
 def genre_recommend(request):
     #장르 > 장르id > 영화 id
-    result_set = [] 
+    resultsort = [] 
     genre_name = request.data.get('genre_name')
     genre = Genre.objects.filter(name=genre_name).values()
     genre_id = genre[0]['id']
@@ -147,11 +172,11 @@ def genre_recommend(request):
     query_set = Movie_genre_list.objects.filter(genre_id_id=genre_id).values()
     for row in query_set :
         mid = row['movie_id_id']
-        mid_filter_list = Movie.objects.filter(id = mid).values() ##쿼리셋 리스트
+        mid_filter_list = Movie.objects.filter(id = mid).values('id').distinct() ##쿼리셋 리스트
         for kk in mid_filter_list.values_list():
             # print(mid_filter_list)
             # mid_filter_list = list(mid_filter_list)
-            # result_set.append(mid_filter_list)
+            # resultsort.append(mid_filter_list)
             
             add_set = {
                     "title" : kk[1],
@@ -164,6 +189,102 @@ def genre_recommend(request):
                     "overview" : kk[8]
                 }
             # print(add_set)
-            result_set.append(add_set)
-    result_set = sorted(result_set, key=lambda result_set: result_set["vote_average"], reverse= True)        
-    return JsonResponse({"message" : result_set},status=200)    
+            resultsort.append(add_set)
+    resultsort = sorted(resultsort, key=lambda result_set: (result_set["vote_average"],result_set['release_date']), reverse=True  )      
+    result_set =[]
+    if(len(resultsort)>=20):    
+        for i in range(20):
+            result_set.append(resultsort[i])
+    return JsonResponse({"message" : result_set},status=200)   
+
+#영화 장르 추출
+def extract_genre(movieid):
+    tmdb_api = TMDB_API('d9560dbd976c10837a85763f71ab79b1')
+    url=tmdb_api.get_url(movieid)
+    movies_dict = requests.get(url)
+    movie = movies_dict.json()
+    set = []
+    for i in range(len(movie['genres'])) :
+            add_set = {
+                "keyword" : movie['genres'][i]['name']
+            }
+            set.append(add_set)
+    return set
+
+# 영화(movie id) 존재확인
+def movie_exist_check(movieid):
+    tmdb_api = TMDB_API('d9560dbd976c10837a85763f71ab79b1')
+    url=tmdb_api.get_url(movieid)
+    movies_dict = requests.get(url)
+    if(movies_dict.status_code == 401 or movies_dict.status_code == 404):
+        return False
+    else:
+        return True
+
+# TMDB 영화정보요청 API
+class TMDB_API():
+    url = 'https://api.themoviedb.org/3/movie/'
+    key = 'd9560dbd976c10837a85763f71ab79b1'
+
+    def __init__(self,key):
+        self.key = key
+
+    def get_url(self,num):
+        return f'{self.url}{num}?api_key={self.key}&language=ko-KR'
+        #https://api.themoviedb.org/3/movie/3?api_key=d9560dbd976c10837a85763f71ab79b1&language=ko-KR
+
+
+###DB DATA IMPORT / NOW-47PAGE / POPULAR -50PAGE / UPCOMING -18PAGE
+class URLMaker():
+    url = 'https://api.themoviedb.org/3/movie/upcoming'
+    key = 'd9560dbd976c10837a85763f71ab79b1'
+
+    def __init__(self,key):
+        self.key = key
+
+    def get_url(self,num):
+        return f'{self.url}?api_key={self.key}'
+
+def movieInfo():
+    url_maker = URLMaker('d9560dbd976c10837a85763f71ab79b1')
+    page = 1
+    while page < 19 : 
+        url = url_maker.get_url(page)
+        url_1 = f'{url}&language=ko-kr&page={page}'
+        print(url_1)
+        movies_dict = requests.get(url_1)
+        movie = movies_dict.json()
+        print()
+        try:
+            for i in range(len(movie['results'])):
+                m_rst = movie['results'][i]
+                if m_rst['overview'] and m_rst['adult'] == False:
+                    new_movie = Movie()
+                    if(Movie.objects.filter(id=m_rst['id']).exists() == True):
+                        #print('********중복영화id'+str(m_rst[id]))
+                        continue
+                    new_movie.id = m_rst['id']
+                    new_movie.title = m_rst['title']
+                    new_movie.original_title = m_rst['original_title']
+                    poster_path = m_rst['poster_path']
+                    new_movie.poster_path= f'https://image.tmdb.org/t/p/w500/{poster_path}'
+                    new_movie.release_date = m_rst['release_date']
+                    new_movie.runtime = 0   
+                    new_movie.popularity = m_rst['popularity']
+                    new_movie.vote_average = m_rst['vote_average']
+                    new_movie.overview = m_rst['overview']
+
+                    new_movie.save()
+
+                    for k in range(len(m_rst['genre_ids'])):
+                        if(Movie_genre_list.objects.filter(movie_id_id=m_rst['id']).exists() == True):
+                            print('********중복장르id'+str(m_rst[id]))
+                            continue
+                        Movie_genre_list.objects.create(movie_id_id=m_rst['id'], genre_id_id=m_rst['genre_ids'][k])
+                        k += 1
+
+                    
+                    print(str(m_rst['title'])+'처리')
+        except:
+            pass
+        page += 1
